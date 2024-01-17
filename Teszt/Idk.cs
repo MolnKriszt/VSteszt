33d25b7446c09b8d6417ac83d8e4a23a1004f7da
MolnKriszt <krisz200601@gmail.com>
2024-01-17 17:13:42 +0100

    teszt3

---
+++ Teszt/Idk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+    static partial class Idk
+    {
+        public static void lofasz()
+        {
+            Console.WriteLine("\t=\nkutya");
+        }
+    }
+}
